9e17ecf466df7bf00e0fa76705595d29bc2c3a4c
nicko <37054216+nicko-lee@users.noreply.github.com>
2020-06-04 23:00:34 +1000

    Remove unnecessary files from Git tracking + recreate existing API functionality with Express JS
    Much nicer to use Express JS!

--- src/index.js
+++ src/index.js
@@ -1,47 +1,60 @@
-var http = require("http");
-var url = require("url");
+// tutorial based off this: https://www.youtube.com/watch?v=pKd0Rpw7O48
+const express = require("express");
+const app = express(); // loads a top level express app
+
+// This is essentially adding a piece of middleware
+// https://youtu.be/pKd0Rpw7O48?t=1965
+// else at the bottom in the POST request when u try and access re.body.name
+// it will not work
+app.use(express.json()); // enables parsing of JSON in the body of reqs
+
+const PORT = process.env.PORT || 8080;
+
 const dealCards = require("./avalon.js").dealCards;
-var PORT = process.env.PORT || 8080;
 
-//create a server object:
-var server = http.createServer(function(req, res) {
-  var page = url.parse(req.url).pathname;
-  console.log(page);
-  if (page === "/") {
-    res.writeHead(200, {
-      "Content-Type": "text/plain",
-      "Access-Control-Allow-Origin": "*"
-    });
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" }
+];
 
-    res.write("Hi welcome to Avalon.");
-  } else if (page === "/dealCards") {
-    // call "business logic"
-    var dealt = dealCardsAndStringify(10);
-
-    // write HTTP response back to client
-    res.writeHead(200, {
-      "Content-Type": "text/plain",
-      "Access-Control-Allow-Origin": "*"
-    });
-    res.write(dealt);
-  } else {
-    res.writeHead(404, {
-      "Content-Type": "text/plain",
-      "Access-Control-Allow-Origin": "*"
-    });
-    res.write("Sorry page not found");
-  }
-  res.end();
+// takes 2 params a route and a callback function which is the route handler
+app.get("/", (req, res) => {
+  res.send("My goodness");
 });
-server.listen(PORT);
 
-function dealCardsAndStringify(players) {
-  var dealt = dealCards(players);
-  console.log("value of dealt: ", dealt);
+app.post("/", (req, res) => {
+  res.send("This is a POST req");
+});
 
-  // convert map to array of tuples: https://stackoverflow.com/questions/29085197/how-do-you-json-stringify-an-es6-map
-  var jsonText = JSON.stringify(dealt);
-  console.log("value of STRINGIFY dealt: ", jsonText);
+// reading params (which are diff from query string params)
+// query params a way to provide extra optional data to a server on the back of a GET req
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id)); // returns bool
+  if (!course)
+    res.status(404).send("The course with the given id was not found");
+  res.send(course);
+});
 
-  return jsonText;
-}
+// get all courses
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  // create a new course object based on incoming request
+  // then save to "in-memory DB" which is just our empty array
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+// ACTUAL AVALON ENDPOINTS
+app.get("/dealCards", (req, res) => {
+  res.send(dealCards());
+});
+
+app.listen(PORT, () => console.log("Listening on port ", PORT));
